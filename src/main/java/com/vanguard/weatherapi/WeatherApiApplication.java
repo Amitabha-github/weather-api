663861d4d1d6124d522ea04bbfec66f73c708040
Amitabha Sadhukhan <amitabha@amitabhas-mbp.lan>
2021-05-02 00:30:17 +0800

    Add logging changes

--- src/main/java/com/vanguard/weatherapi/WeatherApiApplication.java
+++ src/main/java/com/vanguard/weatherapi/WeatherApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class WeatherApiApplication {
@@ -17,5 +18,15 @@ public class WeatherApiApplication {
 	{
 		
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setIncludeHeaders(false);
+	    return loggingFilter;
 	}
 }
